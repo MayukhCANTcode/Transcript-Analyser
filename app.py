8d957a1b8fff4720898472cc11390ad92005f9e7
Mayukh Das <m4yukhd4s@gmail.com>
2026-06-22 14:39:01 +0530

    Fix Streamlit dependencies

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def load_llm():
     api_key = os.getenv("GOOGLE_API_KEY")
 
     llm = ChatGoogleGenerativeAI(
-        model="gemini-2.5-flash"
+        model="gemini-2.5-flash",
         google_api_key=api_key
     )
 
